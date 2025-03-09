9ecd684ebac4b9a88c29c5fde04fef6ce348fea0
Shadirvan <shadirvan2021@gmail.com>
2025-03-09 08:46:19 +0530

    Add Commandline option instead of Explicit IP Declaration

--- client/hidep.go
+++ client/hidep.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"os"
 	"time"
 )
 
-var knockSequence = []int{1234, 5678, 9101}
+var knockSequence = []int{1256, 3333, 5656}
+var serverIP = flag.String("host", "", "IP address to send the knocks") // No default value
 
-const serverIP = "127.0.0.1"
 const timeout = 2 * time.Second
 
 func sendKnock(port int) error {
-	address := fmt.Sprintf("%s:%d", serverIP, port)
-	conn, err := net.DialTimeout("udp", address, timeout) // Use UDP instead of TCP
+	address := fmt.Sprintf("%s:%d", *serverIP, port)
+	conn, err := net.DialTimeout("udp", address, timeout)
 	if err != nil {
 		return fmt.Errorf("failed to send knock to %s: %v", address, err)
 	}
@@ -30,6 +32,17 @@ func sendKnock(port int) error {
 }
 
 func main() {
+	// Parse the command-line arguments
+	flag.Parse()
+
+	// Check if the host is provided
+	if *serverIP == "" {
+		fmt.Println("Error: -host argument is required")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	fmt.Printf("Sending knock to server: %s\n", *serverIP)
 	for _, port := range knockSequence {
 		err := sendKnock(port)
 		if err != nil {
